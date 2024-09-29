1cb993d05e55f90e4fa21e83a2d9aaaf81b4816a
Nikhil4498 <nikhilcherian3@gmail.com>
2024-09-29 15:24:55 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -9,11 +9,11 @@ class Calculator:
 
     def multiply(self, a, b):
         return a * b
-
-   def divide(a, b):
-    if b == 0:
-        return "Error: Division by zero is not allowed."
-    return a / b
+        
+    def divide(a, b):
+       if b == 0:
+           return "Error: Division by zero is not allowed."
+        return a / b
 
     def square_root(self, x):
         return math.sqrt(x)
