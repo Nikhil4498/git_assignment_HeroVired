8460b2924d2ecea1153abb45decd8274efc7e1ae
Nikhil4498 <nikhilcherian3@gmail.com>
2024-09-29 15:28:08 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -10,7 +10,7 @@ class Calculator:
     def multiply(self, a, b):
         return a * b
         
-    def divide(a, b):
+    def divide(self, a, b):
        if b == 0:
            return "Error: Division by zero is not allowed."
         
